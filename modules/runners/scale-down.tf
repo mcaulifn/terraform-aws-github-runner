65535fbea5fb3c0b9c9fc05b2ce83c950079ef64
Nathaniel McAuliffe <nmcauliffe@expediagroup.com>
2021-06-25 14:49:21 -0400

    Removing KMS/GH Auth from scale-down

--- modules/runners/scale-down.tf
+++ modules/runners/scale-down.tf
@@ -14,13 +14,8 @@ resource "aws_lambda_function" "scale_down" {
   environment {
     variables = {
       ENVIRONMENT                     = var.environment
-      KMS_KEY_ID                      = var.encryption.kms_key_id
       ENABLE_ORGANIZATION_RUNNERS     = var.enable_organization_runners
       MINIMUM_RUNNING_TIME_IN_MINUTES = var.minimum_running_time_in_minutes
-      GITHUB_APP_KEY_BASE64           = local.github_app_key_base64
-      GITHUB_APP_ID                   = var.github_app.id
-      GITHUB_APP_CLIENT_ID            = var.github_app.client_id
-      GITHUB_APP_CLIENT_SECRET        = local.github_app_client_secret
       SCALE_DOWN_CONFIG               = jsonencode(var.idle_config)
       GHES_URL                        = var.ghes_url
     }
